8695d34e23f3dfcfe226e42a2a4b5b9ffaa29063
Grant Rogers <grant.e.rogers@gmail.com>
2017-02-08 11:34:52 +0100

    Add files via upload

--- RunHFReportTool.sh
+++ RunHFReportTool.sh
@@ -1,10 +1,16 @@
 #script to run HFradar docker tool
 
-docker_image_name=tequila_test
-
 report_output_directory_host=/home/grogers/Reports
 
 year=2016
 month=1
 
-docker run -v /data/current/opendap/observational/mooring/currentmeter/:/data/current/opendap/observational/mooring/currentmeter/ -v /home/radar/data_rt/radar_system_eivissa:/home/radar/data_rt/radar_system_eivissa -v $report_output_directory_host:/HFRadarReports/Reports -it --dns=172.16.135.60 --dns=172.16.135.61 $docker_image_name $year $month
+docker run \
+-v /data/current/opendap/observational/mooring/currentmeter/:/data/current/opendap/observational/mooring/currentmeter/ \
+-v /home/radar/data_rt/radar_system_eivissa:/home/radar/data_rt/radar_system_eivissa \
+-v $report_output_directory_host:/HFRadarReports/Reports \
+-it --dns=172.16.135.60 --dns=172.16.135.61 \
+--name hfradarcontainer \
+socib/hfradarreports $year $month
+
+##remove docker container
