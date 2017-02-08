1666deddd3d1cd97c081718d8cc2132a7058cac6
Grant Rogers <grogers@socib.es>
2017-02-08 12:42:14 +0100

    Bash Update

--- RunHFReportTool.sh
+++ RunHFReportTool.sh
@@ -1,9 +1,21 @@
+#!/bin/bash
 #script to run HFradar docker tool
 
-report_output_directory_host=/home/grogers/Reports
+report_output_directory_host=$HOME/Reports
 
-year=2016
-month=3
+if [ $# -lt 2 ]; then
+  echo "\nNot enough arguments!\n"
+  echo "Usage: RunHRReportTool.sh yyyy mm"
+  exit 
+elif [ $# -gt 2 ]; then
+  echo "\nToo many arguments!\n"
+  echo "Usage: RunHRReportTool.sh yyyy mm"
+  exit 
+fi
+
+#take arguments passed to script
+year=$1
+month=$2
 
 docker run \
 -v /data/current/opendap/observational/mooring/currentmeter/:/data/current/opendap/observational/mooring/currentmeter/ \
@@ -14,3 +26,4 @@ docker run \
 socib/hfradarreports $year $month
 
 ##remove docker container
+docker rm hfradarcontainer
